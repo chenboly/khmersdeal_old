a642f3b444a77267fe22c4ae0be43f9848406d70
chenboly <chenboly@gmail.com>
2018-11-12 10:48:05 +0700

    Initial commit

---
+++ src/main/java/com/khmersdeal/khmersdeal/KhmersdealApplication.java
@@ -0,0 +1,12 @@
+package com.khmersdeal.khmersdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhmersdealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhmersdealApplication.class, args);
+    }
+}
